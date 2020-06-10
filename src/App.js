02503bc6117d7feb09fe74fdf76aa70b048a34a2
ZAIBA0930 <zaibajavaid@gmail.com>
2020-06-09 23:59:28 -0700

    submit file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import My_practice from './practice_file.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <My_practice dishName="Chicken Shwarma" sweetDish="Custurd"/>
+
+        <hr/>
+
+        <My_practice dishName="Chicken Nihari" sweetDish="jalebi"/>
+
+        <hr/>
+
+        <My_practice dishName="Chicken Biryani" sweetDish="Kheer"/>
     </div>
   );
 }
